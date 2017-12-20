318c54cc3e3812efaae09083c6b25418102729ca
James Doc <jamesdoc@gmail.com>
2017-12-20 12:39:21 +0000

    Point at https api

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://www.globe.church/api/one21')
+    fetch('https://www.globe.church/api/one21')
       .then(res => res.json())
       .then(studies => {
         this.setState({
