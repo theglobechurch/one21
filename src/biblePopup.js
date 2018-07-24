60594b088753abc50a7ed673bdf5506955adc49c
James Doc <jamesdoc@gmail.com>
2018-07-24 19:42:25 +0100

    Fix the iOS scrolling bug

--- src/biblePopup.js
+++ src/biblePopup.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import './style/BiblePopup.css';
+import PropTypes from "prop-types";
+import "./style/BiblePopup.css";
 
 class BiblePopup extends Component {
-
   render() {
     return (
       <div className="biblePopup">
-        <button
-          className="btn"
-          onClick={this.props.toggleBiblePopup}
-        >
+        <button className="btn" onClick={this.props.toggleBiblePopup}>
           Close
         </button>
-        <iframe
-          className="biblePopup__iframe"
-          title={`Bible: ` + this.props.passage}
-          marginWidth='0'
-          marginHeight='0' 
-          frameBorder='0' 
-          src={ `https://www.biblegateway.com/passage/?search=` + this.props.passage + `&version=NIVUK` }
-        />
+        <div className="biblePopup__iframe">
+          <iframe
+            className="biblePopup__iframe__int"
+            title={`Bible: ` + this.props.passage}
+            marginWidth="0"
+            marginHeight="0"
+            frameBorder="0"
+            src={
+              `https://www.biblegateway.com/passage/?search=` +
+              this.props.passage +
+              `&version=NIVUK`
+            }
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
 
 BiblePopup.propTypes = {
   passage: PropTypes.string.isRequired,
   toggleBiblePopup: PropTypes.func.isRequired
-}
+};
 
 export default BiblePopup;
