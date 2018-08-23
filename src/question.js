a369a9ac9ebefa28fc08f50690cf1d04bce92f3a
James Doc <jamesdoc@gmail.com>
2018-08-23 12:45:48 +0100

    Tweak padding on scripture

--- src/question.js
+++ src/question.js
@@ -3,47 +3,45 @@ import PropTypes from "prop-types";
 import ExpandableText from "./expandableText";
 
 export default class Question extends Component {
-
   componentDidMount() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
     }
   }
 
   render() {
-    const { itemNo,  question } = this.props;
+    const { itemNo, question } = this.props;
     window.scrollTo(0, 0);
     return (
       <section className="study__question question">
-       
-        { question.type === 'question' && (
+        {question.type === "question" && (
           <header className="question__header">
-            <span>Question { itemNo }:</span>
+            <span>Question {itemNo}:</span>
             <h2>{question.lead}</h2>
           </header>
         )}
-       
-        { question.type === 'pause' && (
+
+        {question.type === "pause" && (
           <header className="question__header">
             <span>Pause</span>
             <h2>{question.body}</h2>
           </header>
         )}
 
-        { question.followup && (
+        {question.followup && (
           <ul className="question__subquestions">
             {question.followup.map((q, i) => (
-              <li key={i}>{ q }</li>
+              <li key={i}>{q}</li>
             ))}
           </ul>
         )}
 
-        { this.props.scripture && (
-          <section>
+        {this.props.scripture && (
+          <section className="question__scripture">
             <h2 className="dinky_title">{this.props.passage}</h2>
             <ExpandableText
               expanded={true}
@@ -51,10 +49,9 @@ export default class Question extends Component {
               text={this.props.scripture}
             />
           </section>
-        ) }
-
+        )}
       </section>
-    )
+    );
   }
 }
 
@@ -63,4 +60,4 @@ Question.propTypes = {
   itemNo: PropTypes.string.isRequired,
   passage: PropTypes.string,
   scripture: PropTypes.array
-}
+};
