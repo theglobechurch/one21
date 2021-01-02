e1ce1853160132736ab9c715a96d28587684ca57
kbberker <kbbuk90@gmail.com>
2021-01-02 20:58:12 +0000

    refactor: biblePopup to func component

--- src/biblePopup.js
+++ src/biblePopup.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./style/BiblePopup.css";
 
-class BiblePopup extends Component {
-  render() {
-    return (
-      <div className="biblePopup">
-        <button className="btn" onClick={this.props.toggleBiblePopup}>
-          Close
-        </button>
-        <div className="biblePopup__iframe">
-          <iframe
-            className="biblePopup__iframe__int"
-            title={`Bible: ${this.props.passage}`}
-            marginWidth="0"
-            marginHeight="0"
-            frameBorder="0"
-            src={
+// class BiblePopup extends Component {
+const BiblePopup = ({ passage, toggleBiblePopup }) => (
+  <div className="biblePopup">
+    <button className="btn" type="button" onClick={toggleBiblePopup}>
+      Close
+    </button>
+    <div className="biblePopup__iframe">
+      <iframe
+        className="biblePopup__iframe__int"
+        title={`Bible: ${passage}`}
+        marginWidth="0"
+        marginHeight="0"
+        frameBorder="0"
+        src={
               `https://www.biblegateway.com/passage/?search=${
-                this.props.passage
+                passage
               }&version=NIVUK`
             }
-          />
-        </div>
-      </div>
-    );
-  }
-}
+      />
+    </div>
+  </div>
+);
 
 BiblePopup.propTypes = {
   passage: PropTypes.string.isRequired,
