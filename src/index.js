a7398e9cfa81276e5c657fc6d71532b14821785a
James Doc <james@globe.church>
2024-10-14 21:49:43 +0100

    Restore service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+serviceWorkerRegistration.register();
